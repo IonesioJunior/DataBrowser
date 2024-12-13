091f178b08238bcaa05d687241fed43a9d0a8ebf
IonesioJunior <ionesiojr@gmail.com>
2024-12-13 00:16:10 -0300

    Updating data query

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 from pathlib import Path
-from syftbox.lib import Client 
+from syftbox.lib import Client
 import os
 import json
+import yaml
 
 
-def get_datasets_metadata(path: str):
-    datasets_path = Path(path) / "api_data" / "data_loader" / "datasets.json"
+def collect_dataset_metadata(
+    path: str, global_datasets_index: dict, owner: str
+) -> None:
+    datasets_path = Path(path) / "public" / "datasets.yaml"
     if Path(datasets_path).exists():
-        with open(str(datasets_path), "r") as json_file:
-            datasets = json.load(json_file)
+        with open(str(datasets_path), "r") as yaml_file:
+            datasets = yaml.safe_load(yaml_file)
 
         # print(datasets)
         for dataset in datasets["datasets"]:
-            datasets["datasets"][dataset]["owner"] = datasets["owner"]
+            dataset_name = dataset["name"]
+            if dataset["name"] in global_datasets_index.keys():
+                global_datasets_index[dataset_name]["owners"].append(owner)
+            else:
+                global_datasets_index[dataset_name] = {
+                    "name": dataset_name,
+                    "description": dataset["description"],
+                    "format": dataset["format"],
+                }
 
-        return datasets["datasets"]
-
-    return None
+                global_datasets_index[dataset_name]["owners"] = [owner]
 
 
 def build_datasites_index(client: Client):
 
     datasites = os.listdir(client.datasites)
 
-    datasite_paths = [str(client.datasites / datasite) for datasite in datasites]
+    datasite_paths = [
+        (str(client.datasites / datasite), datasite) for datasite in datasites
+    ]
     global_dict = {}
-    for datasite in datasite_paths:
-        datasite_datasets = get_datasets_metadata(datasite)
-        if datasite_datasets is not None:
-            global_dict = global_dict | datasite_datasets
+    for datasite, owner in datasite_paths:
+        datasite_datasets = collect_dataset_metadata(datasite, global_dict, owner)
 
     return global_dict
 
@@ -37,11 +46,12 @@ def main():
     client = Client.load()
 
     datasets = build_datasites_index(client)
-    
+
     output_file = client.my_datasite / "public" / "data_search" / "datasets.json"
-    
-    with open(output_file, 'w')  as json_file:
+
+    with open(output_file, "w") as json_file:
         json.dump(datasets, json_file, indent=4)
+
 
 if __name__ == "__main__":
     main()
