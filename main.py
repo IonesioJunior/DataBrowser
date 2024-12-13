5a3d7801971bca34d05c237c70043c862aea369e
IonesioJunior <ionesiojr@gmail.com>
2024-12-13 12:29:38 -0300

    Adding html/css/js assets

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ import json
 import yaml
 
 
+def copy_html_files(source: Path, destination: Path):
+    """
+    Moves all files from the source directory to the destination directory.
+
+    Args:
+        source (Path): The source directory.
+        destination (Path): The destination directory.
+
+    Raises:
+        ValueError: If source or destination is not a directory.
+    """
+    if not source.is_dir():
+        raise ValueError(f"Source {source} is not a directory.")
+    if not destination.exists():
+        destination.mkdir(parents=True)
+    elif not destination.is_dir():
+        raise ValueError(f"Destination {destination} is not a directory.")
+
+    for item in source.iterdir():
+        if item.is_file():
+            target = destination / item.name
+            try:
+                os.rename(item, target)
+            except Exception as e:
+                print(f"Error moving file {item} to {target}: {e}")
+
+
 def collect_dataset_metadata(
     path: str, global_datasets_index: dict, owner: str
 ) -> None:
@@ -46,6 +73,11 @@ def main():
     client = Client.load()
 
     datasets = build_datasites_index(client)
+
+    copy_html_files(
+        source=Path("./assets"),
+        destination=client.my_datasite / "public" / "data_search",
+    )
 
     output_file = client.my_datasite / "public" / "data_search" / "datasets.json"
 
